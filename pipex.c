b87bbe44c69f1257dcec5af8e5106f9d4a717ee2
Lillian Gottschalk <lgottsch@c2r8s5.42prague.com>
2024-11-03 17:36:35 +0100

    nov 3

--- pipex.c
+++ pipex.c
@@ -6,21 +6,67 @@
 /*   By: lgottsch <lgottsch@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 13:54:06 by lgottsch          #+#    #+#             */
-/*   Updated: 2024/11/02 17:35:57 by lgottsch         ###   ########.fr       */
+/*   Updated: 2024/11/03 17:34:11 by lgottsch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "pipex.h"
+
 /*
+???
+when to pipe when to fork? how many simultaneous processes needed?
+
 TO DO
 
-figure out what to do
+1.create pipe: 
+	need fd[2];
+	pipe(fd);
+	
+	fork()
+	if in child do sth
+	else in parent do sth
 
+child = first cmd
+parent = 2nd
+
+
+execute shell command with execve()
+dup2 (duplicate some fds)
+
+waitpid() in main process to only finish after all others are finished
+
+simulating |  https://www.youtube.com/watch?v=6xbLgZpOBi8&list=PLfqABt5AS4FkW5mOn2Tn9ZZLLDwA3kZUY&index=23
 
 */
 
-#include "pipex.h"
+//    ./pipex file1 cmd1 cmd2 file2
 
-int main(void)
+int main(int argc, char * arrgv[])
 {
+	int fd[2]; //fds for pipe
+	int id;
+
+	id = fork(); //parent is some nr, child = 0
+	if (id == 0) //we are in child 
+	{
+		close(fd[0]); //close end of pipe not needed
+		write(fd[1],   ); //write sth to pipe
+		close(fd[1]); //close one finished
+		
+	}
+	else //in parent
+	{
+		close(fd[1]);
+		read(fd[0],     ); //read from pipe
+		
+		close(fd[0]);
+		
+	}
+
+
+
+
+
+	
 	ft_printf("hello\n");
 }
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: lgottsch <lgottsch@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 17:10:31 by lgottsch          #+#    #+#             */
-/*   Updated: 2024/11/02 17:33:23 by lgottsch         ###   ########.fr       */
+/*   Updated: 2024/11/03 15:25:25 by lgottsch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 #include <stdio.h>
 #include "./full_libft/full_libft.h"
+#include <sys/types.h> //fork
 
 
 
